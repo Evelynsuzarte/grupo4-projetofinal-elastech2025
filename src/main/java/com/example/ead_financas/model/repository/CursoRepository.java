4daa641df8d8e1503275faca585680b03d4b76ef
saraalmada <saraalmadaj5@gmail.com>
2025-10-15 11:04:20 -0300

    adição de response entity para todos os endpoints

--- src/main/java/com/example/ead_financas/model/repository/CursoRepository.java
+++ src/main/java/com/example/ead_financas/model/repository/CursoRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.ead_financas.model.entity.Curso;
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
-	List<Curso> findByProfessorId(Long professorId);
-	List<Curso> findByAlunoId(Long idAluno);
+	List<Curso> findByProfessor_Id(Long professorId);
+	List<Curso> findByMatriculas_Aluno_Id(Long idAluno);
 }
 
